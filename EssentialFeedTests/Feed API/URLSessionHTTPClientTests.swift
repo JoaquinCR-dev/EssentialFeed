ed5323116f502fb26ccb46a0c097f5a43ecc297a
Joaquín C <joaquin.corugedo@i3television.es>
2024-12-12 11:25:41 +0100

    Remove redundant test

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -21,15 +21,6 @@ class URLSessionHTTPCLient {
 
 final class URLSessionHTTPClientTests: XCTestCase {
 
-    func test_getFromURL_createsDataTaskWithURL() {
-        let url = URL(string: "http://any-url.com")!
-        let session = URLSessionSpy()
-        let sut = URLSessionHTTPCLient(session: session)
-        sut.get(from: url)
-
-        XCTAssertEqual(session.receivedURLs, [url])
-    }
-
     func test_getFromURL_resumesDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
         let session = URLSessionSpy()
@@ -47,21 +38,20 @@ final class URLSessionHTTPClientTests: XCTestCase {
     // MARK: -  Helpers
 
     private class URLSessionSpy: URLSession, @unchecked Sendable {
-        var receivedURLs = [URL]()
+
         private var stubs = [URL: URLSessionDataTask]()
         func stub(url: URL, task: URLSessionDataTask) {
             stubs[url] = task
         }
 
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionDataTask {
-            receivedURLs.append(url)
             return stubs[url] ?? FakeURLSessionDataTask()
         }
     }
 
     private class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable {
         override func resume() {
-            
+
         }
     }
 
